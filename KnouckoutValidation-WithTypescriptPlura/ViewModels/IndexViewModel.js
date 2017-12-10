4797b10fc55999cdb2c75945320f7541e7a1f8aa
Jean <jeean.llopes@outlook.com>
2017-12-10 17:55:48 -0200

    Add validation

--- KnouckoutValidation-WithTypescriptPlura/ViewModels/IndexViewModel.js
+++ KnouckoutValidation-WithTypescriptPlura/ViewModels/IndexViewModel.js
@@ -1,13 +1,24 @@
+/// <reference path="../scripts/typings/knockout.validation/knockout.validation.d.ts" />
+/// <reference path="../scripts/typings/knockout/knockout.d.ts" />
 var kjs;
 (function (kjs) {
     var validation;
     (function (validation) {
         var IndexViewModel = (function () {
             function IndexViewModel() {
+                //    public FavoriteSport: KnockoutObservable<string> = ko.observable("Futeball")
+                //        .extend({ required: true });
+                //}
+                this.FavoriteSport = ko.observable("Futeball");
+                this.SetupValidation();
             }
+            ;
+            IndexViewModel.prototype.SetupValidation = function () {
+                this.FavoriteSport.extend({ required: true });
+            };
+            ;
             return IndexViewModel;
         }());
         validation.IndexViewModel = IndexViewModel;
     })(validation = kjs.validation || (kjs.validation = {}));
 })(kjs || (kjs = {}));
-//# sourceMappingURL=IndexViewModel.js.map
--- KnouckoutValidation-WithTypescriptPlura/ViewModels/IndexViewModel.ts
+++ KnouckoutValidation-WithTypescriptPlura/ViewModels/IndexViewModel.ts
@@ -1,6 +1,20 @@
-﻿
+﻿/// <reference path="../scripts/typings/knockout.validation/knockout.validation.d.ts" />
+/// <reference path="../scripts/typings/knockout/knockout.d.ts" />
+
 module kjs.validation {
     export class IndexViewModel {
-        //public NoValidation: KnockoutObservable<string> = ko.observable("");
+        //    public FavoriteSport: KnockoutObservable<string> = ko.observable("Futeball")
+        //        .extend({ required: true });
+        //}
+
+        public FavoriteSport: KnockoutObservable<string> = ko.observable("Futeball");
+
+        constructor() {
+            this.SetupValidation();
+        };
+
+        private SetupValidation() {
+            this.FavoriteSport.extend({ required: true });
+        };
     }
 }
