db02fb1b08fe436bcb7fb4cf52e8e87f6f4acb0b
Jean <jeean.llopes@outlook.com>
2017-12-11 00:22:25 -0200

    new class

--- KnouckoutValidation-WithTypescriptPlura/ViewModels/IndexViewModel.js
+++ KnouckoutValidation-WithTypescriptPlura/ViewModels/IndexViewModel.js
@@ -10,11 +10,29 @@ var kjs;
                 //        .extend({ required: true });
                 //}
                 this.FavoriteSport = ko.observable("Futeball");
+                this.FullName = ko.observable("Test Do Not Book");
+                this.Age = ko.observable(1);
+                this.IfEqualValue = ko.observable("");
+                this.IfNotEqualValue = ko.observable("");
+                this.NonObjectValue = ko.observable("");
                 this.SetupValidation();
             }
             ;
             IndexViewModel.prototype.SetupValidation = function () {
                 this.FavoriteSport.extend({ required: true });
+                this.FullName.extend({ required: true });
+                //this.Age.extend({ required: true, min: 5, max: 17 });
+                this.Age.extend({ required: true })
+                    .extend({ min: { params: 5, message: "Must be 4" } })
+                    .extend({ max: { params: 17, message: "Mest be 17" } });
+                //EXPECT IS "Futeball"
+                this.IfEqualValue.extend({ equal: this.FavoriteSport });
+                this.IfNotEqualValue.extend({
+                    notEqual: {
+                        params: this.FavoriteSport, message: "The Value is equal to Futeball"
+                    }
+                });
+                this.NonObjectValue.extend({ equal: { params: "Red", message: "value expect is 'Red'" } });
             };
             ;
             return IndexViewModel;
--- KnouckoutValidation-WithTypescriptPlura/ViewModels/IndexViewModel.ts
+++ KnouckoutValidation-WithTypescriptPlura/ViewModels/IndexViewModel.ts
@@ -6,8 +6,12 @@ module kjs.validation {
         //    public FavoriteSport: KnockoutObservable<string> = ko.observable("Futeball")
         //        .extend({ required: true });
         //}
-
         public FavoriteSport: KnockoutObservable<string> = ko.observable("Futeball");
+        public FullName: KnockoutObservable<string> = ko.observable("Test Do Not Book");
+        public Age: KnockoutObservable<number> = ko.observable(1);
+        public IfEqualValue: KnockoutObservable<string> = ko.observable("");
+        public IfNotEqualValue: KnockoutObservable<string> = ko.observable("");
+        public NonObjectValue: KnockoutObservable<string> = ko.observable("");
 
         constructor() {
             this.SetupValidation();
@@ -15,6 +19,20 @@ module kjs.validation {
 
         private SetupValidation() {
             this.FavoriteSport.extend({ required: true });
+            this.FullName.extend({ required: true });
+            //this.Age.extend({ required: true, min: 5, max: 17 });
+            this.Age.extend({ required: true })
+                .extend({ min: { params: 5, message: "Must be 4" } })
+                .extend({ max: { params: 17, message: "Mest be 17" } })
+
+            //EXPECT IS "Futeball"
+            this.IfEqualValue.extend({ equal: this.FavoriteSport });
+            this.IfNotEqualValue.extend({
+                notEqual: {
+                    params: this.FavoriteSport,message: "The Value is equal to Futeball"
+                }
+            })
+            this.NonObjectValue.extend({ equal: { params: "Red",message: "value expect is 'Red'"}})
         };
     }
 }
